6ef116ee35ce6b07b13815d86ca61e7df7e862ba
Miguel Serrano <miguel.serrano@nextdigital.es>
2024-12-26 16:17:53 +0100

    :art: style: simplify code

--- commands.py
+++ commands.py
@@ -18,9 +18,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def record_results(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name = update.message.from_user.first_name
 
-    # Verificar si el usuario está autorizado
     if validate_name(user_name):
-        # Explicar cómo enviar los tiempos para los dos juegos
         await update.message.reply_text(
             "Para registrar los resultados, por favor, envía tus tiempos de hoy en Queens y en Tango\n"
             "Primero el tiempo del Queens, después el tiempo del Tango\n"
@@ -39,24 +37,21 @@ async def record_results(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 
-# Función para manejar la respuesta del usuario con los tiempos de los juegos
+# Función para manejar los tiempos de los juegos en distintos formatos
 async def handle_tiempos(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name = update.message.from_user.first_name
     message_text = update.message.text
 
-    # Usamos una expresión regular para capturar los tiempos
+    # Expresión regular para capturar los tiempos
     match = re.match(r"(\d+[:]\d+|\d+)\s+(\d+[:]\d+|\d+)", message_text)
 
     if match:
-        # Extraemos los dos tiempos, uno para cada juego
         queens_time = match.group(1)
         tango_time = match.group(2)
 
-        # Convertimos los tiempos de MM:SS o SS a solo segundos
         queens_seconds = convert_to_seconds(queens_time)
         tango_seconds = convert_to_seconds(tango_time)
 
-        # Guardar los resultados en la base de datos, reemplazando el registro si ya existe
         save_results(user_name, "tango", tango_seconds)
         save_results(user_name, "queens", queens_seconds)
 
@@ -73,58 +68,42 @@ async def handle_tiempos(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def results(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Obtener el ranking de hoy
     ranking_queens = get_ranking("queens")
     ranking_tango = get_ranking("tango")
-
-    # Obtener el ranking histórico de victorias y los mejores tiempos
     ranking_historico, tango_victories, queens_victories = get_historical_ranking()
-
-    # Obtener los "Top Precoces"
     best_queens_time, best_queens_players, best_tango_time, best_tango_players = get_top_precoces()
-
-    # Obtener los tiempos promedio por jugador
     avg_times_dict = get_average_times()
 
-    # Procesar los resultados de hoy
     ranking_hoy_message = "Ranking de hoy:\n"
     if ranking_queens or ranking_tango:
-        # Generar el mensaje del ranking para Queens
         ranking_message_queens = "\nRanking Queens:\n"
         for idx, (nombre, tiempo) in enumerate(ranking_queens, start=1):
             ranking_message_queens += f"{idx}. {tiempo}s {nombre}\n"
 
-        # Generar el mensaje del ranking para Tango
         ranking_message_tango = "\nRanking Tango:\n"
         for idx, (nombre, tiempo) in enumerate(ranking_tango, start=1):
             ranking_message_tango += f"{idx}. {tiempo}s {nombre}\n"
 
-        # Combinar ambos rankings en un solo mensaje
         ranking_hoy_message += f"{ranking_message_queens}\n{ranking_message_tango}"
     else:
         ranking_hoy_message += "No hay resultados registrados hoy.\n"
 
-    # Mostrar el ganador de cada edición de Tango
     historic_message = "\nTango Dancers: 🕺\n"
     for idx, (nombre, victorias) in enumerate(tango_victories, start=1):
         historic_message += f"{idx}. {nombre}: {victorias}\n"
 
-    # Mostrar el ganador de cada edición de Queens
     historic_message += "\nKings of Queens: 👑\n"
     for idx, (nombre, victorias) in enumerate(queens_victories, start=1):
         historic_message += f"{idx}. {nombre}: {victorias}\n"
 
-    # Procesar el ranking histórico de victorias
     historic_message += "\nDancing Queens: 🏆\n"
     for idx, (nombre, victorias) in enumerate(ranking_historico, start=1):
         historic_message += f"{idx}. {nombre}: {victorias}\n"
 
-    # Añadir los "Top Precoces" al mensaje
     precoces_message = "\nTop Precoces: ⚡️🥵⌛️\n"
     precoces_message += f"Queens: {best_queens_time}s ({', '.join(best_queens_players)})\n"
     precoces_message += f"Tango: {best_tango_time}s ({', '.join(best_tango_players)})\n"
 
-    # Mostrar los tiempos promedio de Queens ordenados de menor a mayor
     avg_queens_message = "\nPromedio Queens:\n"
     sorted_queens = sorted(avg_times_dict.items(), key=lambda x: x[1]["avg_queens"] if x[1]["avg_queens"] != "N/A" else float('inf'))
     for nombre, tiempos in sorted_queens:
@@ -132,7 +111,6 @@ async def results(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if avg_queens_time is not None:
             avg_queens_message += f"{avg_queens_time:.2f}s - {nombre}\n" if avg_queens_time != "N/A" else ""
 
-    # Mostrar los tiempos promedio de Tango ordenados de menor a mayor
     avg_tango_message = "\nPromedio Tango:\n"
     sorted_tango = sorted(avg_times_dict.items(), key=lambda x: x[1]["avg_tango"] if x[1]["avg_tango"] != "N/A" else float('inf'))
     for nombre, tiempos in sorted_tango:
@@ -140,7 +118,6 @@ async def results(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if avg_tango_time is not None:
             avg_tango_message += f"{avg_tango_time:.2f}s - {nombre}\n" if avg_tango_time != "N/A" else ""
 
-    # Enviar los mensajes
     await update.message.reply_text(ranking_hoy_message)
     await update.message.reply_text(historic_message)
     await update.message.reply_text(precoces_message)
@@ -148,11 +125,8 @@ async def results(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(avg_tango_message)
 
 
-# Función para convertir MM:SS o SS a segundos
 def convert_to_seconds(time_str):
-    # Si el tiempo está en formato MM:SS, lo convertimos a segundos
     if ':' in time_str:
         minutes, seconds = time_str.split(':')
         return int(minutes) * 60 + int(seconds)
-    # Si el tiempo está solo en segundos
     return int(time_str)
--- queries.py
+++ queries.py
@@ -1,13 +1,9 @@
 import os
-import sqlite3
 import pendulum
 import psycopg2
 
-DB_NAME = "results.db"
-
 
 def get_start_of_day():
-    # Obtener la fecha actual en la zona horaria de España
     spain_tz = pendulum.timezone('Europe/Madrid')
     now = pendulum.now(spain_tz)
 
@@ -21,7 +17,6 @@ def get_start_of_day():
 
 
 def init_db():
-    # Inicializar la base de datos si no existe
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         user=os.getenv('DATABASE_USER'),
@@ -48,24 +43,15 @@ def init_db():
     conn.close()
 
 
-# Definir la función para guardar los resultados
 def save_results(nombre, juego, tiempo):
-    # Obtener el inicio del día (9 AM del día actual o de ayer si es antes de las 9 AM)
     start_of_day = get_start_of_day().to_datetime_string()
-
-    # Obtener la fecha y hora actual en formato adecuado
     fecha_hora = pendulum.now().to_datetime_string()
-    # Guardar el número de juego correspondiente en la tabla de victorias
-    # Obtener las fechas base de Tango y Queens
-    tango_base_date = pendulum.parse('2024-10-08')  # Fecha de referencia de Tango
-    queens_base_date = pendulum.parse('2024-04-30')  # Fecha de referencia de Queens
-
-    # Calcular el número de días transcurridos desde la fecha base de cada juego
+    tango_base_date = pendulum.parse('2024-10-08')
+    queens_base_date = pendulum.parse('2024-04-30')
     tango_game_number = pendulum.now().diff(tango_base_date).in_days()
     queens_game_number = pendulum.now().diff(queens_base_date).in_days()
     print(f"Nombre: {nombre}, Juego: {juego}, Fecha inicio del día: {start_of_day}")
 
-    # Conectar a la base de datos
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         user=os.getenv('DATABASE_USER'),
@@ -75,28 +61,13 @@ def save_results(nombre, juego, tiempo):
     )
     c = conn.cursor()
 
-    # Eliminar resultados anteriores del mismo día para el usuario
-    try:
-        c.execute('''DELETE FROM public.resultados WHERE nombre = %s AND juego = %s AND fecha_hora >= %s''', (nombre, juego, start_of_day))
-    except Exception as e:
-        print(f"Error ejecutando la consulta: {e}")
-        conn.rollback()
+    c.execute('''DELETE FROM public.resultados WHERE nombre = %s AND juego = %s AND fecha_hora >= %s''', (nombre, juego, start_of_day))
+    c.execute('''INSERT INTO public.resultados (nombre, juego, numero_juego, tiempo, fecha_hora)
+        VALUES (%s, %s, %s, %s, %s)''', (nombre, juego, tango_game_number, tiempo, fecha_hora))
+    conn.commit()
 
-    # Insertar los nuevos resultados en la tabla 'resultados'
-    if juego == "tango":
-        c.execute('''INSERT INTO public.resultados (nombre, juego, numero_juego, tiempo, fecha_hora)
-                    VALUES (%s, %s, %s, %s, %s)''', (nombre, juego, tango_game_number, tiempo, fecha_hora))
-        conn.commit()
-    else:
-        c.execute('''INSERT INTO public.resultados (nombre, juego, numero_juego, tiempo, fecha_hora)
-                    VALUES (%s, %s, %s, %s, %s)''', (nombre, juego, queens_game_number, tiempo, fecha_hora))
-        conn.commit()
-
-    # Eliminar las victorias anteriores del mismo día para el usuario en ambos juegos
     c.execute('''DELETE FROM public.victorias WHERE numero_juego = %s AND juego = 'tango' ''', (tango_game_number,))
     c.execute('''DELETE FROM public.victorias WHERE numero_juego = %s AND juego = 'queens' ''', (queens_game_number,))
-
-    # Guardar los nuevos resultados en la tabla 'victorias'
     c.execute('''INSERT INTO public.victorias (nombre, juego, numero_juego, tiempo)
         SELECT nombre, juego, numero_juego, tiempo FROM (
         SELECT nombre, juego, numero_juego, tiempo, ROW_NUMBER() OVER (PARTITION BY juego, numero_juego ORDER BY tiempo) AS rn FROM public.resultados)
@@ -106,16 +77,13 @@ def save_results(nombre, juego, tiempo):
         SELECT nombre, juego, numero_juego, tiempo, ROW_NUMBER() OVER (PARTITION BY juego, numero_juego ORDER BY tiempo) AS rn FROM public.resultados)
         WHERE numero_juego = %s AND juego = 'tango' and rn = 1 ''', (tango_game_number,))
 
-    # Confirmar los cambios y cerrar la conexión
     conn.commit()
     conn.close()
 
 
 def get_ranking(juego):
-    # Obtener el inicio del día (9 AM del día actual o de ayer si es antes de las 9 AM)
     start_of_day = get_start_of_day().to_datetime_string()
 
-    # Conectar a la base de datos y obtener el ranking desde el inicio del día hasta ahora
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         user=os.getenv('DATABASE_USER'),
@@ -125,7 +93,6 @@ def get_ranking(juego):
     )
     c = conn.cursor()
 
-    # Ejecutar la consulta SQL para obtener los resultados del día
     c.execute("SELECT nombre, tiempo FROM public.resultados WHERE fecha_hora >= %s AND juego = %s ORDER BY tiempo ASC", (start_of_day, juego))
     ranking_hoy = c.fetchall()
     conn.close()
@@ -134,7 +101,6 @@ def get_ranking(juego):
 
 
 def get_historical_ranking():
-    # Conectar a la base de datos
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         user=os.getenv('DATABASE_USER'),
@@ -144,7 +110,6 @@ def get_historical_ranking():
     )
     c = conn.cursor()
 
-    # Obtener los mejores tiempos para cada número de juego de Tango
     c.execute('''SELECT nombre, COUNT(*) as victorias
         FROM public.victorias
         WHERE juego = 'tango'
@@ -152,7 +117,6 @@ def get_historical_ranking():
         ORDER BY victorias DESC''')
     tango_victories = c.fetchall()
 
-    # Obtener los mejores tiempos para cada número de juego de Queens
     c.execute('''SELECT nombre, COUNT(*) as victorias
         FROM public.victorias
         WHERE juego = 'queens'
@@ -160,19 +124,17 @@ def get_historical_ranking():
         ORDER BY victorias DESC''')
     queens_victories = c.fetchall()
 
-    # Ahora sumamos las victorias por jugador
     c.execute('''SELECT nombre, COUNT(*) as victorias
         FROM public.victorias
         GROUP BY nombre
         ORDER BY victorias DESC''')
     ranking = c.fetchall()
-
     conn.close()
+
     return ranking, tango_victories, queens_victories
 
 
 def get_top_precoces():
-    # Conectar a la base de datos
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         user=os.getenv('DATABASE_USER'),
@@ -182,29 +144,20 @@ def get_top_precoces():
     )
     c = conn.cursor()
 
-    # Obtener el menor tiempo en Queens
     c.execute('''SELECT MIN(tiempo) FROM public.victorias WHERE juego = 'queens' ''')
     best_queens_time = c.fetchone()[0]
-
-    # Obtener el menor tiempo en Tango
     c.execute('''SELECT MIN(tiempo) FROM public.victorias WHERE juego = 'tango' ''')
     best_tango_time = c.fetchone()[0]
-
-    # Obtener todos los jugadores que lograron el mejor tiempo en Queens
     c.execute('''SELECT nombre FROM public.victorias WHERE juego = 'queens' AND tiempo = %s''', (best_queens_time,))
     best_queens_players = [row[0] for row in c.fetchall()]
-
-    # Obtener todos los jugadores que lograron el mejor tiempo en Tango
     c.execute('''SELECT nombre FROM public.victorias WHERE juego = 'tango' AND tiempo = %s''', (best_tango_time,))
     best_tango_players = [row[0] for row in c.fetchall()]
-
     conn.close()
 
     return best_queens_time, best_queens_players, best_tango_time, best_tango_players
 
 
 def get_average_times():
-    # Conectar a la base de datos y obtener los tiempos promedio por juego
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         user=os.getenv('DATABASE_USER'),
@@ -214,7 +167,6 @@ def get_average_times():
     )
     c = conn.cursor()
 
-    # Consultar los tiempos promedio para 'queens' y 'tango'
     c.execute('''
         SELECT nombre,
                AVG(CASE WHEN juego = 'queens' THEN tiempo END) AS avg_queens,
@@ -223,11 +175,8 @@ def get_average_times():
         GROUP BY nombre
     ''')
     average_times = c.fetchall()
-
-    conn.close()
-
-    # Crear un diccionario con los tiempos promedio por jugador
     avg_times_dict = {nombre: {"avg_queens": avg_queens, "avg_tango": avg_tango}
                       for nombre, avg_queens, avg_tango in average_times}
+    conn.close()
 
     return avg_times_dict
